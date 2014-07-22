34ea478dd158c1e616a6e7a2c298c3cd6a2ae386
Allison Larson <allisonlarson91@gmail.com>
2014-07-22 11:21:12 -0600

    Iterations finshed working through tagging

---
+++ lib/idea_box/idea.rb
@@ -0,0 +1,37 @@
+class Idea
+  include Comparable
+  attr_reader :title, :description, :rank, :id, :tags
+
+  def initialize(attributes = {})
+    @title       = attributes["title"]
+    @description = attributes["description"]
+    @rank        = attributes["rank"] || 0
+    @id          = attributes["id"]
+    @tags        = attributes["tags"]
+  end
+
+  def save
+    IdeaStore.create(to_h)
+  end
+
+  # def tag(all_tags)
+  #   all_tags.split(',') unless all_tags.nil?
+  # end
+
+  def to_h
+    {
+      "title" => title,
+      "description" => description,
+      "rank" => rank,
+      "tags" => tags
+    }
+  end
+
+  def like!
+    @rank += 1
+  end
+
+  def <=>(other)
+    other.rank <=> rank
+  end
+end
